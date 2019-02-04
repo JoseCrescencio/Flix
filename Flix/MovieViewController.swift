b20733c8556a6d4d787fb538b1779ab1ab583054
JoseCrescencio <jcrescencio@csumb.edu>
2019-02-04 15:31:45 -0800

    Part 2 50% done

--- Flix/MovieViewController.swift
+++ Flix/MovieViewController.swift
@@ -57,7 +57,6 @@ class MovieViewController: UIViewController, UITableViewDataSource , UITableView
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
-        print( baseUrl + posterPath)
         let posterUrl = URL(string: baseUrl + posterPath)
         
         
@@ -68,14 +67,25 @@ class MovieViewController: UIViewController, UITableViewDataSource , UITableView
         return cell
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath  = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
 
 }
