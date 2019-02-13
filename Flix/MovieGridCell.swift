4af7eeea7cf329d8e3aad1d7138aaa9cdeccf562
JoseCrescencio <jcrescencio@csumb.edu>
2019-02-12 22:56:38 -0800

    final push

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jose Crescencio on 2/12/19.
+//  Copyright © 2019 Jose Crescencio. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
